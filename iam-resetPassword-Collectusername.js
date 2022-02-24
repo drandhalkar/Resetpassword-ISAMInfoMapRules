88581f21c7deaa73874cf580b2c9ab17e5f5d1a2
Devendra Andhalkar <52698558+drandhalkar@users.noreply.github.com>
2022-02-24 15:47:33 +0300

    Update iam-resetPassword-Collectusername.js

--- iam-resetPassword-Collectusername.js
+++ iam-resetPassword-Collectusername.js
@@ -1,11 +1,5 @@
-/*********************************************************************
- *   Licensed Materials - Property of IBM
- *   (C) Copyright IBM Corp. 2016. All Rights Reserved
- *
- *   US Government Users Restricted Rights - Use, duplication, or
- *   disclosure restricted by GSA ADP Schedule Contract with
- *   IBM Corp.
- *********************************************************************/
+
+
 
 importPackage(Packages.com.ibm.security.access.scimclient);
 importClass(Packages.com.ibm.security.access.recaptcha.RecaptchaClient);
